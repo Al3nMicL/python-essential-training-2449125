1325391ef924d3c564a25a178a681ff2b1891652
Al3nMicL <al3nmicl@users.noreply.github.com>
2022-12-27 03:02:30 +0000

    termcolor 01_06_challenge

--- exercise_files/hello.py
+++ exercise_files/hello.py
@@ -1,3 +1,5 @@
-
+import sys
+from termcolor import colored, cprint # pip install termcolor first
 # Print "hello, world!" to the terminal
-print('Hello, World!')
+# print('Hello, World!')
+cprint('Hello, World!', "white", "on_blue")
